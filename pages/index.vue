622a731cd824eef5445c5390ae99e441d992f486
Sergei Koshel <s.koshel@smartinmedia.com>
2020-09-25 18:01:57 +0200

    Demo for NUXT SSR redirection loop

---
+++ pages/index.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        nuxt-ssr-redirection-loop
+      </h1>
+      <div class="links">
+        <a
+          href="/redirection/no-loop"
+          class="button--green"
+        >
+          Redirect with no loop
+        </a>
+        <a
+          href="/redirection/with loop"
+          class="button--red"
+        >
+          Redirect with loop
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ pages/redirection/_code.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Refresh the page with F5. There must be no redirection. But there is, if there is a space or a non-ASCII character in the URL.
+  </div>
+</template>
+
+<script>
+export default {
+  asyncData({ redirect, route }) {
+    if (process.server) {
+      // Redirect to the same route
+      redirect(route);
+    }
+
+    return {};
+  }
+}
+</script>
